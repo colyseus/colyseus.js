9f6eab987aacd2a3acb41900b4d7b43febda2255
Endel Dreyer <endel.dreyer@gmail.com>
2018-05-04 23:55:54 -0300

    add auto-ping to avoid loosing connections. bump v0.9.3

--- src/Connection.ts
+++ src/Connection.ts
@@ -1,9 +1,14 @@
 import WebSocketClient from '@gamestdio/websocket';
 import * as msgpack from 'notepack.io';
 
+import { Protocol } from './Protocol';
+
+const PING_INTERVAL = 15 * 1000; // send ping each 15 seconds.
+
 export class Connection extends WebSocketClient {
 
     private _enqueuedCalls: any[] = [];
+    private _pingInterval: number;
 
     constructor(url, query: any = {}) {
         super(url);
@@ -14,11 +19,18 @@ export class Connection extends WebSocketClient {
     public onOpenCallback(event) {
         super.onOpenCallback();
 
+        this._pingInterval = setInterval(() => this.ping(), PING_INTERVAL) as any;
+
         if (this._enqueuedCalls.length > 0) {
             for (const [method, args] of this._enqueuedCalls) {
                 this[method].apply(this, args);
             }
         }
+    }
+
+    public onCloseCallback(event) {
+        super.onCloseCallback();
+        clearInterval(this._pingInterval);
     }
 
     public send(data: any): void {
@@ -32,6 +44,10 @@ export class Connection extends WebSocketClient {
             // Enqueue data to be sent when readyState == OPEN
             this._enqueuedCalls.push(['send', [data]]);
         }
+    }
+
+    protected ping() {
+        this.send([Protocol.PING]);
     }
 
 }
--- src/Protocol.ts
+++ src/Protocol.ts
@@ -18,4 +18,6 @@ export enum Protocol {
     // Generic messages (50~60)
     BAD_REQUEST = 50,
 
+    // Utils (100~127)
+    PING = 100,
 }
