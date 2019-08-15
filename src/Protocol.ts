6f39ff7ca8bc4570934ba2bf571d016775636f3d
Endel Dreyer <endel.dreyer@gmail.com>
2019-08-14 23:41:02 -0300

    remove unused protocol messages. colyseus/colyseus#176

--- src/Protocol.ts
+++ src/Protocol.ts
@@ -1,23 +1,13 @@
 // Use codes between 0~127 for lesser throughput (1 byte)
 
 export enum Protocol {
-    // User-related (0~8)
-    USER_ID = 1,
-
-    // Room-related (9~19)
-    JOIN_REQUEST = 9,
+    // Room-related (10~19)
     JOIN_ROOM = 10,
     JOIN_ERROR = 11,
     LEAVE_ROOM = 12,
     ROOM_DATA = 13,
     ROOM_STATE = 14,
     ROOM_STATE_PATCH = 15,
-
-    // Match-making related (20~29)
-    ROOM_LIST = 20,
-
-    // Generic messages (50~60)
-    BAD_REQUEST = 50,
 }
 
 export function utf8Read(view: DataView, offset: number) {
