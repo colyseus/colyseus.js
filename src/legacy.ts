e61fd6f254d46a4ad388e6ca80ee2e58cfecb7d8
Endel Dreyer <endel.dreyer@gmail.com>
2021-04-26 19:43:50 -0300

    compatibility: add globalThis if not defined. #86

--- src/legacy.ts
+++ src/legacy.ts
@@ -10,3 +10,13 @@ if (!ArrayBuffer.isView) {
         return a !== null && typeof (a) === 'object' && a.buffer instanceof ArrayBuffer;
     };
 }
+
+// Define globalThis if not available.
+// https://github.com/colyseus/colyseus.js/issues/86
+if (
+    typeof (globalThis) === "undefined" &&
+    typeof (window) !== "undefined"
+) {
+    // @ts-ignore
+    window['globalThis'] = window;
+}
