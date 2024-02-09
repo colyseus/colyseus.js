2c4fdb9a1dce47248b4dff9228c1df2471cc2e15
Endel Dreyer <endel.dreyer@gmail.com>
2024-02-09 13:39:46 -0300

    workaround for 'Access is denied' for localStorage. #139

--- src/Storage.ts
+++ src/Storage.ts
@@ -10,18 +10,26 @@ let storage: any;
 
 function getStorage(): Storage {
     if (!storage)  {
-        storage = (typeof (cc) !== 'undefined' && cc.sys && cc.sys.localStorage)
-            ? cc.sys.localStorage  // compatibility with cocos creator
-            : typeof (window) !== "undefined" && window.localStorage //RN does have window object at this point, but localStorage is not defined
-                ? window.localStorage // regular browser environment
-                : { // mock localStorage for Node.js or RN environment
-                    cache: {},
-                    setItem: function(key, value) { this.cache[key] = value; },
-                    getItem: function(key) { this.cache[key]; },
-                    removeItem: function(key) { delete this.cache[key]; },
-                };
+        try {
+            storage = (typeof (cc) !== 'undefined' && cc.sys && cc.sys.localStorage)
+                ? cc.sys.localStorage  // compatibility with cocos creator
+                : window.localStorage; // RN does have window object at this point, but localStorage is not defined
 
+        } catch (e) {
+            // ignore error
+        }
     }
+
+    if (!storage) {
+        // mock localStorage if not available (Node.js or RN environment)
+        storage = {
+            cache: {},
+            setItem: function (key, value) { this.cache[key] = value; },
+            getItem: function (key) { this.cache[key]; },
+            removeItem: function (key) { delete this.cache[key]; },
+        };
+    }
+
     return storage;
 }
 
