a9d6ec16fba716b0fcf26abc8873f013b66dfc5d
Endel Dreyer <endel.dreyer@gmail.com>
2025-03-25 09:40:04 -0300

    upgrade rollup packages. fix bad embed of 'ws' module on dist file.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,13 @@
+import path from "path";
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import alias from '@rollup/plugin-alias';
 import replace from '@rollup/plugin-replace';
 
-import pkg from "./package.json";
-import schemapkg from "./node_modules/@colyseus/schema/package.json";
+// import as json
+import pkg from "./package.json" with { type: "json" };
+import schemapkg from "./node_modules/@colyseus/schema/package.json" with { type: "json" };
 
 const external = Object.keys(pkg.dependencies);
 
@@ -14,15 +16,15 @@ const bannerStatic = `// colyseus.js@${pkg.version} (@colyseus/schema ${schemapk
 
 const replacePlugin = replace({
     'process.env.VERSION': JSON.stringify(pkg.version),
+    preventAssignment: true,
 });
 
 export default [
 
     // https://github.com/microsoft/TypeScript/issues/18442#issuecomment-749896695
     {
-        preserveModules: true,
         input: ['src/index.ts'],
-        output: [{ banner, dir: 'build/esm', format: 'esm', entryFileNames: '[name].mjs', sourcemap: true },],
+        output: [{ preserveModules: true, banner, dir: 'build/esm', format: 'esm', entryFileNames: '[name].mjs', sourcemap: true },],
         external,
         plugins: [
             replacePlugin,
@@ -31,9 +33,8 @@ export default [
     },
 
     {
-        preserveModules: true,
         input: ['src/index.ts'],
-        output: [{ banner, dir: 'build/cjs', format: 'cjs', entryFileNames: '[name].js', sourcemap: true },],
+        output: [{ preserveModules: true, banner, dir: 'build/cjs', format: 'cjs', entryFileNames: '[name].js', sourcemap: true },],
         external,
         plugins: [
             replacePlugin,
@@ -43,10 +44,10 @@ export default [
 
     // browser/embedded dependencies
     {
-        preserveModules: false,
         input: ['src/index.ts'],
         output: [
             {
+                preserveModules: false,
                 banner: bannerStatic,
                 dir: 'dist',
                 name: "Colyseus",
@@ -62,13 +63,13 @@ export default [
             alias({
                 entries: [
                     // httpie: force `fetch` for web environments
-                    { find: 'httpie', replacement: './node_modules/httpie/fetch/index.js' },
+                    { find: 'httpie', replacement: path.resolve('./node_modules/httpie/fetch/index.js') },
 
                     // ws: force browser.js version.
-                    { find: 'ws', replacement: './node_modules/ws/browser.js' },
+                    { find: 'ws', replacement: path.resolve('./node_modules/ws/browser.js') },
 
                     // @colyseus/schema: force browser version.
-                    { find: '@colyseus/schema', replacement: './node_modules/@colyseus/schema/build/umd/index.js' },
+                    { find: '@colyseus/schema', replacement: path.resolve('./node_modules/@colyseus/schema/build/umd/index.js') },
                 ]
             }),
             commonjs(),
@@ -78,10 +79,10 @@ export default [
 
     // Cocos Creator SDK (same as browser/embedded, but use XHR instead of fetch)
     {
-        preserveModules: false,
         input: ['src/index.ts'],
         output: [
             {
+                preserveModules: false,
                 banner: `// THIS VERSION USES "XMLHttpRequest" INSTEAD OF "fetch" FOR COMPATIBILITY WITH COCOS CREATOR\n${bannerStatic}`,
                 dir: 'dist',
                 name: "Colyseus",
@@ -97,13 +98,13 @@ export default [
             alias({
                 entries: [
                     // httpie: force XHR implementation on browser/UMD environment
-                    { find: 'httpie', replacement: './node_modules/httpie/xhr/index.js' },
+                    { find: 'httpie', replacement: path.resolve('./node_modules/httpie/xhr/index.js' ) },
 
                     // ws: force browser.js version.
-                    { find: 'ws', replacement: './node_modules/ws/browser.js' },
+                    { find: 'ws', replacement: path.resolve('./node_modules/ws/browser.js' ) },
 
                     // @colyseus/schema: force browser version.
-                    { find: '@colyseus/schema', replacement: './node_modules/@colyseus/schema/build/umd/index.js' },
+                    { find: '@colyseus/schema', replacement: path.resolve('./node_modules/@colyseus/schema/build/umd/index.js') },
                 ]
             }),
             commonjs(),
